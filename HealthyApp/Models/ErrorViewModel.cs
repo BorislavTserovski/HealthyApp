b74ff47d716aac6defdb7289473427f90ebcc60d
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-11-07 16:49:30 +0200

    Add project files.

---
+++ HealthyApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HealthyApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
